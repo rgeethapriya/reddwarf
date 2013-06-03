d7eecd54c86a590c6d71c8c3e2ba2c055c6b825d
Geethapriya Raghavan <geet1006@MJT0WTF1G3.rackspace.corp>
2013-06-03 16:19:49 -0500

    Modifying Backup tests... 06/03/13
    Change-Id: I84fea1ba80069c30c6f7ffd93e42c7468457a490

--- reddwarf/tests/api/backups.py
+++ reddwarf/tests/api/backups.py
@@ -57,60 +57,85 @@ class BackupsBase(object):
         self.dbaas = util.create_dbaas_client(instance_info.user)
 
     def _create_backup(self, backup_name, backup_desc):
-        return instance_info.dbaas.backups.create(backup_name,
+        backup_resp = instance_info.dbaas.backups.create(backup_name,
                                                          instance_info.id,
                                                          backup_desc)
-        # print dir(backup_resp)
-        # print backup_resp.status
-        # print backup_resp.locationRef
-        print dir(instance_info)
-        print instance_info.dbaas_flavor
-        print instance_info.dbaas_flavor_href
-        print instance_info.volume
-        print instance_info.user.tenant_id
+        return backup_resp
 
     def _create_restore(self):
-        # self.dbaas.instances.create(
-        #                     instance_info.name,
-        #                     instance_info.dbaas_flavor_href,
-        #                     instance_info.volume,
-        #                     databases,
-        #                     users,
-        #                     restorePoint=backup_id)
-        #instance_info.dbaas.instances.create(instance_info)
-        pass
+        restorePoint = {"backupRef": backup_resp.id}
+        restore_resp = instance_info.dbaas.instances.create(
+                instance_info.name + "_restore",
+                instance_info.dbaas_flavor_href,
+                instance_info.volume,
+                restorePoint=restorePoint)
+        return restore_resp
 
-    @test
-    def _list_backups_by_tenant(self):
-        pass
+    def _list_backups(self):
+        return instance_info.dbaas.backups.list()
 
-    @test
     def _list_backups_by_instance(self):
-        pass
+        return instance_info.dbaas.instances.backups(instance_info.id)
 
-    @test
     def _delete_backup(self):
-        pass
+        assert_equal(backup_info.id, None, "Backup ID is not found")
+        instance_info.dbaas.backups.delete(backup_info.id)
 
-    @test
+
     def _get_backup_status(self):
         pass
 
+    def _verify_instance_is_active(self):
+        result = instance_info.dbaas.instances.get(instance_info.id)
+        print result.status
+        return result.status == 'ACTIVE'
+
+
 @test(depends_on_classes=[WaitForGuestInstallationToFinish],
       groups=[GROUP_POSITIVE])
-class TestBackupPositive(object):
+class TestBackupPositive(BackupsBase):
 
     @test
     def test_verify_backup(self):
-        pass
+        result = self._create_backup(BACKUP_NAME, BACKUP_DESC)
+        assert_equal(BACKUP_NAME, result.name)
+        assert_equal(BACKUP_DESC, result.description)
+        assert_equal(instance_info.id, result.instance_id)
+        assert_equal('NEW', result.status)
+        instance = instance_info.dbaas.instances.list()[0]
+        assert_equal('BACKUP', instance.status)
+        global backup_info
+        backup_info = result
+        # print dir(backup_info)
+        # print backup_info.locationRef
+        # Timing necessary to make the error occur
+        poll_until(self._verify_instance_is_active, time_out=120,
+                   sleep_time=1)
 
     @test
     def test_restore_backup(self):
-        pass
+        if test_config.auth_strategy == "fake":
+            raise SkipTest("Skipping restore tests for fake mode.")
+        restore_result = self._create_restore()
+        assert_equal(200, instance_info.dbaas.last_http_code)
+        assert_equal("BUILD", restore_result.status)
+        global restore_info
+        restore_info = restore_result
+        global restore_instance_id
+        restore_instance_id = restore_result.id
+        print dir(restore_info)
 
-    @test
-    def test_list_backups_for_tenant(self):
-        pass
+    @test(depends_on_classes=[BackupsBase],
+          runs_after=[test_verify_backup])
+    def test_list_backups(self):
+        result = self._list_backups()
+        assert_equal(1, len(result))
+        backup = result[0]
+        assert_equal(BACKUP_NAME, backup.name)
+        assert_equal(BACKUP_DESC, backup.description)
+        assert_equal(instance_info.id, backup.instance_id)
+        assert_equal('COMPLETED', backup.status)
+        #print backup.status
 
     @test
     def test_list_backups_for_instance(self):
@@ -126,7 +151,19 @@ class TestBackupPositive(object):
 
     @test
     def test_delete_backup(self):
-        pass
+        result = self._delete_backup()
+        assert_equal(202, instance_info.dbaas.last_http_code)
+        def backup_is_gone():
+            pass
+#             result = instance_info.dbaas.instances.backups(instance_info.id)
+#             if len(result) == 0:
+#                 return True
+#             else:
+#                 return False
+#         poll_until(backup_is_gone)
+#         assert_raises(exceptions.NotFound, instance_info.dbaas.backups.get,
+#                       backup_info.id)
+
 
 @test(depends_on_classes=[WaitForGuestInstallationToFinish],
       groups=[GROUP_NEGATIVE])
@@ -167,194 +204,101 @@ class TestBackupNegativeive(object):
 
 
 
-@test(depends_on_classes=[WaitForGuestInstallationToFinish],
-      groups=[GROUP])
-class CreateBackups(object):
+    # @test
+    # def test_backup_create_instance_not_found(self):
+    #     """test create backup with unknown instance"""
+    #     assert_raises(exceptions.NotFound, instance_info.dbaas.backups.create,
+    #                   BACKUP_NAME, 'nonexistent_instance', BACKUP_DESC)
 
-    @test
-    def test_backup_create_instance_not_found(self):
-        """test create backup with unknown instance"""
-        assert_raises(exceptions.NotFound, instance_info.dbaas.backups.create,
-                      BACKUP_NAME, 'nonexistent_instance', BACKUP_DESC)
+#
+#     @test
+#     def test_instance_action_right_after_backup_create(self):
+#         """test any instance action while backup is running"""
+#         assert_unprocessable(instance_info.dbaas.instances.resize_instance,
+#                              instance_info.id, 1)
+#
+#     @test
+#     def test_backup_create_another_backup_running(self):
+#         """test create backup when another backup is running"""
+#         assert_unprocessable(instance_info.dbaas.backups.create,
+#                              'backup_test2', instance_info.id,
+#                              'test description2')
+#
+#     @test
+#     def test_backup_delete_still_running(self):
+#         """test delete backup when it is running"""
+#         result = instance_info.dbaas.backups.list()
+#         backup = result[0]
+#         assert_unprocessable(instance_info.dbaas.backups.delete, backup.id)
+#
+#
 
-    @test
-    def test_backup_create_instance(self):
-        """test create backup for a given instance"""
-        result = instance_info.dbaas.backups.create(BACKUP_NAME,
-                                                    instance_info.id,
-                                                    BACKUP_DESC)
-        print "Instance Info DOT user looks like - "
-        print dir(instance_info.user)
-        print instance_info.user.tenant_id
-        print instance_info.user.tenant
-        assert_equal(BACKUP_NAME, result.name)
-        assert_equal(BACKUP_DESC, result.description)
-        assert_equal(instance_info.id, result.instance_id)
-        assert_equal('NEW', result.status)
-        instance = instance_info.dbaas.instances.list()[0]
-        assert_equal('BACKUP', instance.status)
-        global backup_info
-        backup_info = result
+#
+#     @test
+#     @time_out(60 * 30)
+#     def test_backup_created(self):
+#         # This version just checks the REST API status.
+#         def result_is_active():
+#             backup = instance_info.dbaas.backups.get(backup_info.id)
+#             if backup.status == "COMPLETED":
+#                 return True
+#             else:
+#                 assert_not_equal("FAILED", backup.status)
+#                 return False
+#
+#         poll_until(result_is_active)
+#
+#
 
+#     @test
+#     def test_backup_get(self):
+#         """test get backup"""
+#         backup = instance_info.dbaas.backups.get(backup_info.id)
+#         assert_equal(backup_info.id, backup.id)
+#         assert_equal(backup_info.name, backup.name)
+#         assert_equal(backup_info.description, backup.description)
+#         assert_equal(instance_info.id, backup.instance_id)
+#         assert_equal('COMPLETED', backup.status)
+#
+#
+#
+# @test(depends_on_classes=[RestoreUsingBackup],
+#       runs_after=[RestoreUsingBackup],
+#       groups=[GROUP])
+# class WaitForRestoreToFinish(object):
+#     """
+#         Wait until the instance is finished restoring.
+#     """
+#
+#     @test
+#     @time_out(60 * 32)
+#     def test_instance_restored(self):
+#         if test_config.auth_strategy == "fake":
+#             raise SkipTest("Skipping restore tests for fake mode.")
+#
+#         # This version just checks the REST API status.
+#         def result_is_active():
+#             instance = instance_info.dbaas.instances.get(restore_instance_id)
+#             if instance.status == "ACTIVE":
+#                 return True
+#             else:
+#                 # If its not ACTIVE, anything but BUILD must be
+#                 # an error.
+#                 assert_equal("BUILD", instance.status)
+#                 if instance_info.volume is not None:
+#                     assert_equal(instance.volume.get('used', None), None)
+#                 return False
+#
+#         poll_until(result_is_active)
+#
+#
+# @test(runs_after=[WaitForRestoreToFinish],
+#       groups=[GROUP])
+# class DeleteBackups(object):
+#
+#     @test
+#     def test_backup_delete_not_found(self):
+#         """test delete unknown backup"""
+#         assert_raises(exceptions.NotFound, instance_info.dbaas.backups.delete,
+#                       'nonexistent_backup')
 
-@test(runs_after=[CreateBackups],
-      groups=[GROUP])
-class AfterBackupCreation(object):
-
-    @test
-    def test_instance_action_right_after_backup_create(self):
-        """test any instance action while backup is running"""
-        assert_unprocessable(instance_info.dbaas.instances.resize_instance,
-                             instance_info.id, 1)
-
-    @test
-    def test_backup_create_another_backup_running(self):
-        """test create backup when another backup is running"""
-        assert_unprocessable(instance_info.dbaas.backups.create,
-                             'backup_test2', instance_info.id,
-                             'test description2')
-
-    @test
-    def test_backup_delete_still_running(self):
-        """test delete backup when it is running"""
-        result = instance_info.dbaas.backups.list()
-        backup = result[0]
-        assert_unprocessable(instance_info.dbaas.backups.delete, backup.id)
-
-
-@test(runs_after=[AfterBackupCreation],
-      groups=[GROUP])
-class WaitForBackupCreateToFinish(object):
-    """
-        Wait until the backup create is finished.
-    """
-
-    @test
-    @time_out(60 * 30)
-    def test_backup_created(self):
-        # This version just checks the REST API status.
-        def result_is_active():
-            backup = instance_info.dbaas.backups.get(backup_info.id)
-            if backup.status == "COMPLETED":
-                return True
-            else:
-                assert_not_equal("FAILED", backup.status)
-                return False
-
-        poll_until(result_is_active)
-
-
-@test(depends_on=[WaitForBackupCreateToFinish],
-      groups=[GROUP])
-class ListBackups(object):
-
-    @test
-    def test_backup_list(self):
-        """test list backups"""
-        result = instance_info.dbaas.backups.list()
-        assert_equal(1, len(result))
-        backup = result[0]
-        assert_equal(BACKUP_NAME, backup.name)
-        assert_equal(BACKUP_DESC, backup.description)
-        assert_equal(instance_info.id, backup.instance_id)
-        assert_equal('COMPLETED', backup.status)
-
-    @test
-    def test_backup_list_for_instance(self):
-        """test list backups"""
-        result = instance_info.dbaas.instances.backups(instance_info.id)
-        assert_equal(1, len(result))
-        backup = result[0]
-        assert_equal(BACKUP_NAME, backup.name)
-        assert_equal(BACKUP_DESC, backup.description)
-        assert_equal(instance_info.id, backup.instance_id)
-        assert_equal('COMPLETED', backup.status)
-
-    @test
-    def test_backup_get(self):
-        """test get backup"""
-        backup = instance_info.dbaas.backups.get(backup_info.id)
-        assert_equal(backup_info.id, backup.id)
-        assert_equal(backup_info.name, backup.name)
-        assert_equal(backup_info.description, backup.description)
-        assert_equal(instance_info.id, backup.instance_id)
-        assert_equal('COMPLETED', backup.status)
-
-
-@test(runs_after=[ListBackups],
-      groups=[GROUP])
-class RestoreUsingBackup(object):
-
-    @test
-    def test_restore(self):
-        """test restore"""
-        if test_config.auth_strategy == "fake":
-            raise SkipTest("Skipping restore tests for fake mode.")
-        restorePoint = {"backupRef": backup_info.id}
-        result = instance_info.dbaas.instances.create(
-            instance_info.name + "_restore",
-            instance_info.dbaas_flavor_href,
-            instance_info.volume,
-            restorePoint=restorePoint)
-        assert_equal(200, instance_info.dbaas.last_http_code)
-        assert_equal("BUILD", result.status)
-        global restore_instance_id
-        restore_instance_id = result.id
-
-
-@test(depends_on_classes=[RestoreUsingBackup],
-      runs_after=[RestoreUsingBackup],
-      groups=[GROUP])
-class WaitForRestoreToFinish(object):
-    """
-        Wait until the instance is finished restoring.
-    """
-
-    @test
-    @time_out(60 * 32)
-    def test_instance_restored(self):
-        if test_config.auth_strategy == "fake":
-            raise SkipTest("Skipping restore tests for fake mode.")
-
-        # This version just checks the REST API status.
-        def result_is_active():
-            instance = instance_info.dbaas.instances.get(restore_instance_id)
-            if instance.status == "ACTIVE":
-                return True
-            else:
-                # If its not ACTIVE, anything but BUILD must be
-                # an error.
-                assert_equal("BUILD", instance.status)
-                if instance_info.volume is not None:
-                    assert_equal(instance.volume.get('used', None), None)
-                return False
-
-        poll_until(result_is_active)
-
-
-@test(runs_after=[WaitForRestoreToFinish],
-      groups=[GROUP])
-class DeleteBackups(object):
-
-    @test
-    def test_backup_delete_not_found(self):
-        """test delete unknown backup"""
-        assert_raises(exceptions.NotFound, instance_info.dbaas.backups.delete,
-                      'nonexistent_backup')
-
-    @test
-    @time_out(60 * 2)
-    def test_backup_delete(self):
-        """test delete"""
-        instance_info.dbaas.backups.delete(backup_info.id)
-        assert_equal(202, instance_info.dbaas.last_http_code)
-
-        def backup_is_gone():
-            result = instance_info.dbaas.instances.backups(instance_info.id)
-            if len(result) == 0:
-                return True
-            else:
-                return False
-        poll_until(backup_is_gone)
-        assert_raises(exceptions.NotFound, instance_info.dbaas.backups.get,
-                      backup_info.id)
